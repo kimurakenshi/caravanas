23982928a04c9ea9a080544584967a50b1ad726a
Sebastian Giraudo <sebastian.giraudo@globant.com>
2017-05-07 15:36:01 -0700

    Add material UI library

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import Root from './containers/Root';
@@ -7,20 +9,29 @@ import './app.global.css';
 
 const store = configureStore();
 
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
+  <MuiThemeProvider>
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
 
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
     const NextRoot = require('./containers/Root'); // eslint-disable-line global-require
+
     render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
+      <MuiThemeProvider>
+        <AppContainer>
+          <NextRoot store={store} history={history} />
+        </AppContainer>
+      </MuiThemeProvider>,
       document.getElementById('root')
     );
   });
